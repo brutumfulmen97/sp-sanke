6d2dd3cab008b6ef129420e176c4fdc6d34a8963
brutumfulmen97 <109813663+brutumfulmen97@users.noreply.github.com>
2023-10-30 12:27:58 +0100

    mockup gotov

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,67 +3,51 @@
 import Draggable from "@/components/Draggable";
 import Droppable from "@/components/Droppable";
 import { DndContext } from "@dnd-kit/core";
-import {
-    restrictToHorizontalAxis,
-    createSnapModifier,
-    restrictToParentElement,
-} from "@dnd-kit/modifiers";
+import { restrictToHorizontalAxis } from "@dnd-kit/modifiers";
 import { useState } from "react";
 
 export default function Home() {
-    const containers = ["A", "B", "C"];
+    const [value, setValue] = useState(0);
+    const containers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
     const [parent, setParent] = useState(null);
     const draggableMarkup = (
         <Draggable id="draggable">
-            <div className="w-[150px] border-2 border-red bg-gray-200 text-center">
-                Drag Me
-            </div>
+            <div className="w-[150px] text-center">Drag Me</div>
         </Draggable>
     );
 
-    const gridSize = 150; // pixels
-    const snapToGridModifier = createSnapModifier(gridSize);
-
     return (
-        <div className="flex w-[600px] border-2 border-black">
-            <DndContext
-                onDragEnd={handleDragEnd}
-                modifiers={[
-                    restrictToHorizontalAxis,
-                    snapToGridModifier,
-                    restrictToParentElement,
-                ]}
-            >
-                {parent === null ? draggableMarkup : null}
+        <>
+            <div className="flex w-[1500px] border-2 border-black">
+                <DndContext
+                    onDragEnd={handleDragEnd}
+                    modifiers={[restrictToHorizontalAxis]}
+                >
+                    {parent === null ? draggableMarkup : null}
 
-                {containers.map((id) => (
-                    // We updated the Droppable component so it would accept an `id`
-                    // prop and pass it to `useDroppable`
-                    <Droppable
-                        key={id}
-                        id={id}
-                        className="w-[150px] text-center border-2"
-                    >
-                        {parent === id ? draggableMarkup : "Drop here"}
-                    </Droppable>
-                ))}
-            </DndContext>
-        </div>
+                    {containers.map((id) => (
+                        // We updated the Droppable component so it would accept an `id`
+                        // prop and pass it to `useDroppable`
+                        <Droppable
+                            key={id}
+                            id={id}
+                            className="w-[150px] text-center border-2"
+                        >
+                            {parent === id ? draggableMarkup : "Drop here"}
+                        </Droppable>
+                    ))}
+                </DndContext>
+            </div>
+            <h2>{value}ft</h2>
+        </>
     );
 
     function handleDragEnd(event: any) {
         const { over } = event;
 
         setParent(over ? over.id : null);
-    }
-
-    function snapToGrid(args: any) {
-        const { transform } = args;
-
-        return {
-            ...transform,
-            x: Math.ceil(transform.x / gridSize) * gridSize,
-            y: Math.ceil(transform.y / gridSize) * gridSize,
-        };
+        if (over) {
+            setValue(+over.id * 25000);
+        }
     }
 }
